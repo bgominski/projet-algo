501c6cc28c4d5a85a74d117746a268ca10c640d3
Solenn-Boulard <solenn.boulard@insa-lyon.fr>
2020-04-07 18:04:16 +0200

    Modif EcouteurReponses

--- EcouteurReponse.java
+++ EcouteurReponse.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class EcouteurReponse implements ActionListener{
 	private FenetreJeu fen;
     private String rep ; 
-	private IA ia ;
-    private Jeu jeu ; 
+    private Jeu jeu = fen.getJeu(); 
+	private IA ia = new IA(jeu);
 	public EcouteurReponse(FenetreJeu fen, String rep) {
 		this.fen = fen;
         this.rep = rep ; 
@@ -14,13 +14,13 @@ public class EcouteurReponse implements ActionListener{
     //actionPerformed indique les instructions a executer au clic du bouton
     public void actionPerformed(ActionEvent e){
     
-        jeu = fen.getJeu(); 
-        ia = new IA(jeu); /**instancie un IA avec le jeu en cours dans la fenetreJeu donc pas besoin d'instancier jeu ?**/
-        System.out.println("nouvelle boucle");
-        ia.afficheListe();
+        //jeu = fen.getJeu(); 
+        //ia = new IA(jeu); /**instancie un IA avec le jeu en cours dans la fenetreJeu donc pas besoin d'instancier jeu ?**/
+        //System.out.println("nouvelle boucle");
+        //ia.afficheListe();
         Question Query = ia.QuestionIA();
-        System.out.println(Query.toString());
-        System.out.println("Veuillez saisir réponse :");     
+       // System.out.println(Query.toString());
+        //System.out.println("Veuillez saisir réponse :");     
         if(rep.equals("Oui")){
         //Appel de la méthode triant les personnage en fonction de la réponse
         ia.UpdateListes(true,Query);
