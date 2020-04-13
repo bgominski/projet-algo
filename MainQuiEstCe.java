4711e590b0229111996e0e1b13b3f6c069a1419a
Solenn-Boulard <solenn.boulard@insa-lyon.fr>
2020-04-13 18:41:22 +0200

    DEBUG

--- MainQuiEstCe.java
+++ MainQuiEstCe.java
@@ -10,16 +10,10 @@ public class MainQuiEstCe {
 		Jeu jeu = new Jeu(); 
 		//Création de la fenêtre de jeu
         FenetreAccueil f1= new FenetreAccueil(jeu); 	
-<<<<<<< HEAD
-        FenetreJeu f2 = new FenetreJeu(f1);
-        FenetreRejouer f3 = new FenetreRejouer(f2) ; 
-	}
-=======
         //FenetreFinJeu f2 = new FenetreFinJeu();
         //FenetreFinJeu f3 = new FenetreFinJeu() ; 
         }
         
 
 	
->>>>>>> 2a900a3117d64a52c51e551daf2fb731ca339041
 }
