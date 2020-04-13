528c35db415671fd54ab4502a6b3406a62482e38
Emmanuelle-Rsi <62595793+Emmanuelle-Rsi@users.noreply.github.com>
2020-04-13 17:54:51 +0200

    Update MainQuiEstCe.java

--- MainQuiEstCe.java
+++ MainQuiEstCe.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.awt.*;
 import java.awt.Color;
 import javax.swing.*;
+
 public class MainQuiEstCe {
 	
 	public static void main (String[] args) {
@@ -9,7 +10,16 @@ public class MainQuiEstCe {
 		Jeu jeu = new Jeu(); 
 		//Création de la fenêtre de jeu
         FenetreAccueil f1= new FenetreAccueil(jeu); 	
+<<<<<<< HEAD
         FenetreJeu f2 = new FenetreJeu(f1);
         FenetreRejouer f3 = new FenetreRejouer(f2) ; 
 	}
+=======
+        //FenetreFinJeu f2 = new FenetreFinJeu();
+        //FenetreFinJeu f3 = new FenetreFinJeu() ; 
+        }
+        
+
+	
+>>>>>>> 2a900a3117d64a52c51e551daf2fb731ca339041
 }
