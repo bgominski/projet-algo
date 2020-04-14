d08ff0f0c6d94194600df1eb93f9287e13d6277a
Solenn-Boulard <solenn.boulard@insa-lyon.fr>
2020-04-14 15:32:42 +0200

    MAJ écouteur réponse avec IA

--- EcouteurReponse.java
+++ EcouteurReponse.java
@@ -23,9 +23,11 @@ public class EcouteurReponse implements ActionListener{
 	
     //actionPerformed indique les instructions a executer au clic du bouton
     public void actionPerformed(ActionEvent e){
+        
+  
     
         if(rep.equals("Oui")){
-	
+    
         jeu.getIA().UpdateListes(true,jeu.getQuestion()); //Mise à jour des listes selon la réponse
         query = jeu.getIA().QuestionIA(); //génération d'une nouvelle question
         jeu.setQuestion(query);
@@ -34,11 +36,16 @@ public class EcouteurReponse implements ActionListener{
         fen.affichePerso(); //Mise à jour des persos possible
         jeu.playSound("/Users/alainboulard/Desktop/OUI.wav");
         
+            if(jeu.getIA().PersonnageFound()==true){
+                FenetreRejouer fenetreRejouer = new FenetreRejouer(fen) ; 
+            }
+                
+        
         }
          
        
         if(rep.equals("Non")){
-		
+        
         jeu.getIA().UpdateListes(false,jeu.getQuestion()); //Mise à jour des listes selon la réponse
         query = jeu.getIA().QuestionIA(); //génération d'une nouvelle question
         jeu.setQuestion(query);
@@ -46,9 +53,14 @@ public class EcouteurReponse implements ActionListener{
         fen.afficheQuestion(); //Affichage de la question
         fen.affichePerso(); //Mise à jour des persos possible
         jeu.playSound("/Users/alainboulard/Desktop/NON.wav");
+            if(jeu.getIA().PersonnageFound()==true){
+                    FenetreRejouer fenetreRejouer = new FenetreRejouer(fen) ; 
+                }
             
-    }
-    
-	}
-    
+        }
+    } 
 }
+    
+	
+    
+
