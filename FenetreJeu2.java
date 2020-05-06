40dfb79c55bb0ebb17965332a3f1389c1d030b44
Emmanuelle-Rsi <62595793+Emmanuelle-Rsi@users.noreply.github.com>
2020-05-06 08:59:56 +0200

    Merge branch 'master' of https://github.com/bgominski/projet-algo

---
+++ FenetreJeu2.java
@@ -0,0 +1,422 @@
+import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class FenetreJeu2  extends JFrame{
+    //ATTRIBUTS
+    
+    //Conteneur Haut
+    JLabel labelExplication ; 
+    
+    //Conteneur Gauche
+    JTextArea textRegles;
+    
+    //Conteneur Bas
+    JLabel labelNomAuteurs ; //Noms auteurs
+    
+    //Conteneur Centre
+    JPanel listePersos ; //panel central, Affiche la liste des personnages possibles
+    JComboBox boxAttribut; //Contient les attributs à tester
+    JComboBox boxValeur; //Contient les valeurs de l'attribut choisi
+    JButton btnDemander; //Permet de poser un question (MAJ des attributs possibles, des valeur d'attributs possibles et des persos possibles)
+    JPanel panelTerciaire = new JPanel(new FlowLayout()); //Contient les JComboBox et le bouton "demander", passé en attribut car MAJ au cours du jeu
+    JLabel reponse ; //Contient la réponse à la question précédente
+    
+    //Tablaux utilisés dans la JComboBox boxValeur (impossible d'utiliser des ArrayList)
+    private String[] listeGenre=new String[] {"femme","homme"} ; //Genres possibles
+	private String[] listeTypeCheveux = new String[]{"lisses" ,"bouclés", "ondulés", "frisés"};; //CheveuxTypes de cheveux possibles
+	private String[] listeCouleurCheveux =new String[]{"blonds" ,"chatains", "bruns","noirs","bicolores"};; //Couleurs de cheveux possibles
+	private String[] listeCouleurYeux =new String[]{"marrons" ,"bleus", "ondulés"};; //Couleurs d'yeux possibles
+    private String[] listeAccessoire =new String[]{"lunettes", "rien","sa beauté"};; //Accessoires possibles
+    private String[] listePays =new String[]{"France", "Guadeloupe","Pérou","El Salvador","Maroc","Martinique","Guyane","Vénézuela","Panama","Le ciel","Méééxico"}; //Pays possibles
+    private String[] listeNoms =new String[]{"BIKIKRE Gabriel", "BEUGIN Maeva", "BOULARD Solenn", "BEGAZO Gabriel", "CANJURA Sonia", "CLAUDE Edouard", "DRUTEL Maxence", "DAGHNI Samya", "FAURE Ninon", 
+    "GOMINSKI Benjamin", "GRANGE Quentin", "HARDY Guenael", "LENGANEY Nicolas", "RAGOT Andres", "MARTINEZ Karina", "ROUGIER Julia", "ROUSSI Emmanuelle", "SANDOZ Alexandre", "SIMONNET Léa", "VARGAS Diego"}; //Noms possibles
+    
+    //Liste en cours dans boxValeur
+    String[] listeEnCours; //permet de récupérer la liste présente dans boxValeur
+        
+    //Jeu courant
+    Jeu jeu ; 
+    
+<<<<<<< HEAD
+    //Stock de la valeur du selected item de boxValeur
+    String valeurSelectionnee; //permet de tester cette valeur lorsqu'une question est posée
+=======
+    //Stockage de la valeur du selected item Attribut
+    String valeurSelectionnee;
+>>>>>>> 05b19320e34eda3a02448d4470367ecfbff3adc0
+    
+    //Liste des persos possibles à mettre à jour à chaque questions
+    ArrayList<String> listePersonnagePossibles ;
+    
+    //Pour répondre aux questions
+    boolean aAttribut =false; 
+    
+  
+    
+    //CONSTRUCTEUR
+    public FenetreJeu2(Jeu jeu){
+        super("Qui-est-ce ? - Fenêtre de jeu ") ;  //on definit le nom de la fenetre
+        
+        this.jeu=jeu; //récupération du jeu
+  	
+		//Dimensions de la fenetre graphique et fermeture
+        setSize(950, 1200) ; 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ;
+        
+        //===== Instanciation des widgets de la fenêtre ====//
+        
+        //Conteneur Haut
+        labelExplication = new JLabel("Pour poser une question, choisissez la caractéristique grâce aux menus déroulants puis cliquez sur DEMANDER") ;
+        
+        //Conteneur Gauche
+        textRegles= new JTextArea("\n\n\n L'IA te dira alors \n si tu es sur la bonne piste. \nTu pourras donc ensuite cliquer sur \n les personnages que tu souhaites pour\n pour les supprimer") ; 
+        textRegles.setEditable(false);
+        
+        //Conteneur Bas
+        labelNomAuteurs = new JLabel ("Auteurs : BOULARD Solenn, GOMINSKI Benjamin, ROUSSI Emmanuelle");
+        
+        //Conteneur Centre
+        reponse = new JLabel("La réponse à votre question s'affichera ici");
+        int s=jeu.getListePersonnage().size()+1;
+        JPanel listePersos = new JPanel (new GridLayout(s,1)); //On crée une grille  une case par personnage + une case pour afficher les réponses
+        listePersos.add(reponse); //Afficher la réponse à la derière question posée
+        JButton[] tabButton = new JButton[jeu.getListePersonnage().size()]; //On crée un tableau de boutons qui contient autant de cases que de personnages
+        for (int i=0; i<tabButton.length; i++) {
+			tabButton[i]= new JButton(jeu.getListePersonnage().get(i).toString()); //chaque bouton contient la discription d'un personnage
+			tabButton[i].addActionListener(new EcouteurSupprimer()); //chaque bouton doit disparaître quand on clique dessus
+			listePersos.add(tabButton[i]); //chaque case de la grille contient un bouton
+		}
+        
+   
+        String[] types = new String[] {"Genre", "Type de cheveux", "Couleur de cheveux", "Couleur des yeux", "Accessoire", "Pays", "Nom"}; //À remplacer par la liste des attributs
+        boxAttribut = new JComboBox(types);
+        boxAttribut.addActionListener(new EcouteurComboBox(this));     
+        
+        btnDemander = new JButton("DEMANDER"); //Ajouter un écouteur de MAJ
+        btnDemander.addActionListener(new EcouteurDemander(this));
+        
+        
+        //=========== Organisation structurelle ======//
+        
+        //Ajout des widgets du panel terciaire de gauche à droite
+        panelTerciaire.add(boxAttribut);
+        panelTerciaire.add(btnDemander);
+        
+        
+        //Création du panel secondaire
+        JPanel panelSecondaire = new JPanel(new BorderLayout());
+        panelSecondaire.setBackground(new Color(240,255,255)) ;
+        //Ajout des widgets
+        panelSecondaire.add(panelTerciaire,BorderLayout.NORTH);
+        panelSecondaire.add(listePersos,BorderLayout.CENTER);
+        panelSecondaire.add(reponse,BorderLayout.EAST);
+        
+        
+        //Création du panel principal
+        JPanel panelPrincipal = new JPanel(new BorderLayout());
+        //Ajout des widgets
+        panelPrincipal.add(labelExplication,BorderLayout.NORTH);
+        panelPrincipal.add(panelSecondaire,BorderLayout.CENTER);
+        panelPrincipal.add(textRegles,BorderLayout.WEST);
+        panelPrincipal.add(labelNomAuteurs, BorderLayout.SOUTH);
+        
+        //Ajout du conteneur principal à la fenêtre
+        this.add(panelPrincipal);
+        this.setVisible(true);
+         
+        }
+        
+        //MÉTHODES
+             
+    //Getters
+    public Jeu getJeu(){ 
+        return jeu ; 
+    }
+    
+    public String getAttribut(){
+        return (String) boxAttribut.getSelectedItem(); 
+    }
+    
+    public String getValeur(){ 
+        return (String) boxValeur.getSelectedItem();
+     }
+    
+    
+    public String[] getListeGenre(){ 
+		return listeGenre;
+	} 
+	
+    public String[] getListeTypeCheveux(){
+		return listeTypeCheveux;
+	}
+	
+    public String[] getListeCouleurCheveux(){
+		return listeCouleurCheveux;
+	}
+	
+    public String[] getListeCouleurYeux(){
+		return listeCouleurYeux;
+	}
+	
+    public String[] getListeAccessoire(){
+		return listeAccessoire;
+	}
+	
+    public String[] getListePays(){
+		return listePays;
+	}
+	
+	public String[] getListeEnCours(){
+		return listeEnCours;
+	}
+	
+    public String[] getListeNoms(){
+		return listeNoms;
+	}
+        
+	public JComboBox getboxValeur(){
+		return boxValeur;
+	}
+	
+	//Setters
+	public void setListeGenre(String[] tab){ 
+		listeGenre=tab;
+	} 
+	
+    public void setListeTypeCheveux(String[] tab){
+		listeTypeCheveux=tab;
+	}
+	
+    public void setListeCouleurCheveux(String[] tab){
+		listeCouleurCheveux=tab;
+	}
+	
+    public void setListeCouleurYeux(String[] tab){
+		listeCouleurYeux=tab;
+	}
+	
+    public void setListeAccessoire(String[] tab){
+		listeAccessoire=tab;
+	}
+	
+    public void setListePays(String[] tab){
+		listePays=tab;
+	}
+	
+	 public void setListeEnCours(String[] tab){
+		listeEnCours=tab;
+	}
+    
+       
+    //méthode pour rafrachir la 2e comBoBox en fonction de la première
+    public String[] adapterBoxValeur(){
+        
+        String attribut=this.getAttribut();
+        String[] valeurs = {""}; //On initialise le tableau qui contient la liste à afficher
+              
+        switch(attribut) {
+			
+			case "Genre":
+            boxValeur = new JComboBox(getListeGenre()); //Mise à jour du contenue de boxValeur
+            valeurs = getListeGenre(); //Mise à jour de la liste des valeurs testés
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            
+            case "Type de cheveux":
+            boxValeur = new JComboBox(getListeTypeCheveux()); 
+            valeurs = getListeTypeCheveux();
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            
+            case "Couleur de cheveux":
+            boxValeur = new JComboBox(getListeCouleurCheveux()); 
+            valeurs = getListeCouleurCheveux();
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander); 
+            validate(); 
+			break;
+            
+            
+            case "Couleur des yeux":
+            boxValeur = new JComboBox(getListeCouleurYeux()); 
+            valeurs = getListeCouleurYeux();
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            case "Accessoire":
+            boxValeur = new JComboBox(getListeAccessoire()); 
+            valeurs = getListeAccessoire();
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            case "Pays":
+            boxValeur = new JComboBox(getListePays()); 
+            valeurs = getListePays();
+            //Mise à jour du panel
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            case "Nom":
+            boxValeur = new JComboBox(getListeNoms()); 
+            panelTerciaire.removeAll() ; 
+            panelTerciaire.add(boxAttribut);
+            panelTerciaire.add(boxValeur);
+            panelTerciaire.add(btnDemander);
+            validate(); 
+			break;
+            
+            
+        }
+        setListeEnCours(valeurs);
+        return valeurs; 
+    }
+    
+    
+    //Méthode de maj des attributs contenue dans boxValeur en fonction de la question posée
+    public void refreshList(){
+   
+        valeurSelectionnee = (String)boxValeur.getSelectedItem(); //on récupère la valeur sélectionné dans boxValeur
+        String[] tab = getListeEnCours(); //on récupère les valeurs possible avant la question posée
+		ArrayList<String> ListeIntermediaire = new ArrayList<String>(); //on crée une liste intermédiaire
+		for(int i=0; i<tab.length; i++) {
+			ListeIntermediaire.add(tab[i]);//on récupère les éléments présents dans boxValeurs et on les places dans ListeIntermédiaire
+		}
+		
+		ListeIntermediaire.removeIf(s -> (getValeur().equals(s))); //on élimine la valeur sélectionnée (valeur testée par la question) 
+		String[] tabMAJ=new String[ListeIntermediaire.size()]; //On crée un tableau
+		for (int i=0; i<ListeIntermediaire.size(); i++){
+			tabMAJ[i]= ListeIntermediaire.get(i); //On place les éléments de la liste dans le tableau 
+		}
+		
+		boxValeur = new JComboBox(tabMAJ); //On met à jour les éléments de boxValeur grâce à tabMAJ
+		
+		//Mise à jour du panel
+        panelTerciaire.removeAll() ; 
+        panelTerciaire.add(boxAttribut);
+        panelTerciaire.add(boxValeur);
+        panelTerciaire.add(btnDemander); 
+        validate(); 
+        
+        //Mise à jour des listes passées en attribut (pour éviter que les caractéristiques déjà testées réapparaissent)
+		String attribut=this.getAttribut();
+		switch(attribut) {
+			case "Genre":
+			setListeGenre(tabMAJ);
+			break;
+			
+			case "Type de cheveux":
+			setListeTypeCheveux(tabMAJ);
+			break;
+			
+			case "Couleur de cheveux":
+			setListeCouleurCheveux(tabMAJ);
+			break;
+			
+            case "Couleur des yeux":
+            setListeCouleurYeux(tabMAJ);
+			break;
+            
+            case "Accessoire":
+            setListeAccessoire(tabMAJ);
+			break;
+            
+            case "Pays":
+            setListePays(tabMAJ);
+			break;
+        
+		}
+	}
+			
+    //METHODE POUR VERIFIER SI LA CARACTÉRISTIQUE DEMANDÉE CORRESPOND AU PERSONNAGE
+    public String donnerReponse(){
+<<<<<<< HEAD
+        String res = "Non"; //Initialisation de la réponse
+  
+		ArrayList<String> caractéristiquesPerso = new ArrayList<String>(); //on crée une liste 
+        //On stocke toutes les caractéristqieus du perso dans cette liste
+        caractéristiquesPerso.add(jeu.getQui().getGenre()) ;
+        caractéristiquesPerso.add(jeu.getQui().getCouleurCheveux()) ;
+        caractéristiquesPerso.add(jeu.getQui().getTypeCheveux()) ;
+        caractéristiquesPerso.add(jeu.getQui().getAccessoire()) ;
+        caractéristiquesPerso.add(jeu.getQui().getCouleurYeux()) ;
+        caractéristiquesPerso.add(jeu.getQui().getPays()) ;
+        caractéristiquesPerso.add(jeu.getQui().getNom()) ;
+=======
+        String res = "Non"; 
+        System.out.println(jeu.getQui()); 
+        /**TESTS
+         * System.out.println("DEBUT DONNER REPONSE"+valeurSelectionnee); 
+        
+        System.out.println(jeu.getQui()); 
+        System.out.println(jeu.getQui().getGenre());
+        System.out.println(jeu.getQui().getCouleurCheveux());
+        System.out.println(jeu.getQui().getTypeCheveux());
+        System.out.println(jeu.getQui().getAccessoire());
+        System.out.println(jeu.getQui().getCouleurYeux());
+        System.out.println(jeu.getQui().getNom());
+        System.out.println(jeu.getQui().getPays());**/
+        
+        
+         //Stocker toutes les caractéristqieus du perso dans une liste
+        ArrayList<String> caractéristiquesPerso = new ArrayList<String>(); //on crée une liste intermédiaire
+            caractéristiquesPerso.add(jeu.getQui().getGenre()) ; 
+            caractéristiquesPerso.add(jeu.getQui().getCouleurCheveux()) ;
+            caractéristiquesPerso.add(jeu.getQui().getTypeCheveux()) ;
+            caractéristiquesPerso.add(jeu.getQui().getAccessoire()) ;
+            caractéristiquesPerso.add(jeu.getQui().getCouleurYeux()) ;
+            caractéristiquesPerso.add(jeu.getQui().getPays()) ;
+            caractéristiquesPerso.add(jeu.getQui().getNom()) ;
+>>>>>>> 05b19320e34eda3a02448d4470367ecfbff3adc0
+            
+        //On parcourt les éléments de la liste et on renvoie oui s'il y a une correspondance
+		for(String s : caractéristiquesPerso){
+            
+            if(valeurSelectionnee.equals(s)){
+                if(s.equals(jeu.getQui().getNom())){
+                   FenetreRejouer fenRejouer  = new FenetreRejouer(this);
+                }
+                res="oui";
+                aAttribut=true;       
+            }	
+		}  
+		    
+        //On affiche la réponse
+        reponse.setText("Réponse : "+res); 
+        repaint(); 
+        revalidate() ; 
+        return res;
+    }
+    
+    
+}
---
+++ FenetreRejouer.java
@@ -0,0 +1,124 @@
+import javax.swing.*; 
+import java.awt.*;
+import java.awt.event.*;
+
+public class FenetreRejouer  extends JFrame{
+	
+	//ATTRIBUTS
+	
+<<<<<<< HEAD
+	FenetreJeu fenetreJeu; //contient la fenêtre de jeu courante
+	Jeu jeu; //contient le jeu courant
+	JLabel jeuFini; //affiche un message de fin de jeu
+=======
+	FenetreJeu fenetreJeu; 
+    FenetreJeu2 fenetreJeu2 ; 
+	Jeu jeu; //jeu courant
+	JLabel jeuFini; 
+>>>>>>> 05b19320e34eda3a02448d4470367ecfbff3adc0
+	JTextArea descriptionPerso; //affiche la description du perosnnage choisi au début
+	JLabel imagePerso; //affiche l'image du perosnnage choisi au début
+	JButton btnRejouer; //permet de lancer une nouvelle partie
+	
+	public FenetreRejouer(FenetreJeu fenetreJeu){
+		super("Qui-est-ce ? - Fin de jeu "); //on definit le nom de la fenêtre
+		jeu=fenetreJeu.getJeu(); //on écupère le jeu courant
+		
+		//Dimensions et fermetrure de la fenêtre
+		setSize(1280, 1024) ; 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; 
+        
+        //Ajout du fond d'écran
+        setContentPane(new AfficheImage("/Users/alainboulard/Desktop/Image Algo/fond-colore-confettis-qui-est-baisse-illustration-vectorielle_41084-389.png")); 
+        getContentPane().setLayout(null);
+        getContentPane().setBackground(new Color(187,210,225)) ; 
+		
+		//===== Instanciation des widgets de la fenêtre ====//
+		jeuFini=new JLabel("Notre IA t'as démasqué... Aurais-tu un crush sur:"); //message de fin de jeu
+			
+		descriptionPerso=new JTextArea("Nom: "+jeu.getQui().getNom()+"\nGenre: "+
+        jeu.getQui().getGenre()+"\nCouleur cheveux: "+jeu.getQui().getCouleurCheveux()
+        +"\nType cheveux: "+jeu.getQui().getTypeCheveux() +"\nYeux: "+jeu.getQui().getCouleurYeux()); //description du personnage deviné
+        
+        descriptionPerso.setOpaque(false); 
+        descriptionPerso.setEditable(false);
+        descriptionPerso.setFont(new Font("Arial", Font.BOLD, 30)); 
+        
+		imagePerso=new JLabel(jeu.getQui().getImage()); 
+		btnRejouer=new JButton("Rejouer !!");
+		btnRejouer.addActionListener(new EcouteurRejouer(this));
+      
+		
+		//=========== Organisation structurelle ======//
+		
+		//On défini la taille et la position des composants
+        jeuFini.setBounds(500, 250, 500, 100); 
+        descriptionPerso.setBounds(370, 380, 600,800); 
+        imagePerso.setBounds(700, 310, 400, 300); 
+        btnRejouer.setBounds(560, 700, 150, 35); 
+        
+        //On ajoute les composants à la fenêtre
+        getContentPane().add(jeuFini);
+        getContentPane().add(descriptionPerso);
+        getContentPane().add(imagePerso);
+        getContentPane().add(btnRejouer);
+		
+		// Fenêtre visible
+		setVisible(true);
+	}
+    
+    
+    public FenetreRejouer(FenetreJeu2 fenetreJeu2){
+		super("Qui-est-ce ? - Fin de jeu "); //on definit le nom de la fenêtre
+        
+		jeu=fenetreJeu2.getJeu();
+		
+		//Dimensions et fermetrure de la fenêtre
+		setSize(1280, 1024) ; 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; 
+        
+        //Ajout du fond d'écran
+        setContentPane(new AfficheImage("/Users/alainboulard/Desktop/Image Algo/fond-colore-confettis-qui-est-baisse-illustration-vectorielle_41084-389.png")); 
+        /**getContentPane().setLayout(new BorderLayout()); **/
+        getContentPane().setLayout(null);
+        getContentPane().setBackground(new Color(187,210,225)) ; 
+		
+		//===== Instanciation des widgets de la fenêtre ====//
+		jeuFini=new JLabel("Bravooo tu as vaincu l'IA ! ");
+		descriptionPerso=new JTextArea("Nom: "+jeu.getQui().getNom()+"\nGenre: "+
+        jeu.getQui().getGenre()+"\nCouleur cheveux: "+jeu.getQui().getCouleurCheveux()
+        +"\nType cheveux: "+jeu.getQui().getTypeCheveux() +"\nYeux: "+jeu.getQui().getCouleurYeux());
+        descriptionPerso.setOpaque(false); 
+        descriptionPerso.setEditable(false);
+        descriptionPerso.setFont(new Font("Arial", Font.BOLD, 30));
+		imagePerso=new JLabel(jeu.getQui().getImage()); //À REMPLACER PAR L'IMAGE DE 'QUI'
+		btnRejouer=new JButton("Rejouer !!");
+		btnRejouer.addActionListener(new EcouteurRejouer(this));
+      
+		
+		//=========== Organisation structurelle ======//
+		
+		//JPanel panelPrincipal=new JPanel(new BorderLayout()); //panel contenant tous les widgets
+        /**getContentPane().add(jeuFini,BorderLayout.NORTH);
+        getContentPane().add(imagePerso,BorderLayout.WEST);
+        getContentPane().add(descriptionPerso,BorderLayout.CENTER);
+        getContentPane().add(btnRejouer,BorderLayout.SOUTH);**/
+         
+        jeuFini.setBounds(500, 250, 500, 100); 
+        descriptionPerso.setBounds(370, 380, 600,800); 
+        imagePerso.setBounds(700, 310, 400, 300); 
+        btnRejouer.setBounds(560, 700, 150, 35); 
+        
+        
+        getContentPane().add(jeuFini);
+        getContentPane().add(descriptionPerso);
+        getContentPane().add(imagePerso);
+        getContentPane().add(btnRejouer);
+       
+        
+		
+		// Fenêtre visible
+		setVisible(true);
+	}
+
+}
