c0721ddbcca67f5ea8786a9d1ed35b0305eee85e
bgominski <benjamin.gominski@insa-lyon.fr>
2020-05-07 14:38:28 +0200

    Revert "qsd"
    This reverts commit cf6ccaa5591af784d97ec971063d847fbf3b4bd3.

--- IA.java
+++ IA.java
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * IA.java   used by Jeu.java
- * 
- * Copyright 2020 GOMINSKI BENJAMIN
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
- 
- // LAST EDIT : 07/05/2020 14:20
- 
- /*
-  * Lexique : 
-  * Attributs :  pour désigner les attributs d'un personnage, comme les yeux, les cheveux...
-  * Valeur : pour désigner la valeur de l'attribut, comme vert, bleu, blanc...
-  * 
-  * 
-  */
-  
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
 import java.util.*;
 
 public class IA {
@@ -56,29 +20,13 @@ public class IA {
     
     private ArrayList<String> ListeAccessoire= new ArrayList<String>();
     private ArrayList<String> ListePays= new ArrayList<String>();
-<<<<<<< HEAD
-=======
-    
-    /* La première version comprenait l'attribut couleur peau que nous avons par la suite enlevé
-     */
-	//private ArrayList<String> ListeCouleurPeau=new ArrayList<String>();
-    
-    
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
 	//Indexation de ces listes pour une utilisation plus rapide
     private ArrayList<ArrayList<String>> ListeListe= new ArrayList<ArrayList<String>>();
     
   
     
     
-	/* Constructeur
-     * 
-     * @param : jeu de type Jeu
-     * 
-     * Crée la liste de personnage possibles : c'est le "pool" des choix pour l'ordinateur
-     * 
-     * Utilise méthode MAJListes */
-     
+	//Constructeur
 	public IA(Jeu jeu){
 		
 		this.ListePersonnageInit=(ArrayList<Personnage>)(jeu.getListePersonnage().clone());
@@ -88,46 +36,22 @@ public class IA {
         this.MAJListes(ListePersonnageInit);
 		    
 	}
-    
 	
-<<<<<<< HEAD
 	//CHOISIR LA QUESTION A POSER
-=======
-	/* Méthode principale : 
-     * 
-     * Aucun paramètre.
-     * 
-     * Renvoie une Question, cf Question.java 
-     */
-     
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
 	public Question QuestionIA(){
        
         
         //On détermine la liste la plus petite 
-<<<<<<< HEAD
         int compteur=20;            //valeur pour initialiser
-=======
-     
-        // && indiceListe!=(ListeListe.size()-1) : condition pour éviter IndexOutofBounds 
-        
-        int compteur=100;            //valeur maximale non atteinte pour initialiser
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
         int indiceListe=0;
         for(ArrayList A : ListeListe){
-            
-              
-            //Si deux listes de taille égales, la liste choisie sera celle avec l'indice le plus élevé
+              //Si une liste ne comporte qu'1 élément,la question ne peut pas porter dessus
+                
             if( A.size()<=compteur && indiceListe!=(ListeListe.size()-1) ){
                 compteur=A.size();
                 indiceListe++;
                 
                 }
-            /* Si une liste ne comporte qu'1 élément,la question ne peut pas porter dessus
-             * 
-             * De plus, on s'assure que l'indice choisi n'est pas OutOfBounds dans ListeListe
-             */
-             
             if (A.size()==1 && indiceListe!=(ListeListe.size()-1)){
                 indiceListe++;
             }
@@ -139,60 +63,32 @@ public class IA {
         
             
         }
-        
+        System.out.println("indiceListe= "+indiceListe);
        
         
-<<<<<<< HEAD
         //On choisit un élément dans liste d'indice indiceListe
-=======
-        /* On choisit un élément dans liste d'indice indiceListe
-         * 
-         * 1ère version : utilisation de l'objet Random, qui s'avère ne pas être nécessaire
-         * 
-         * Random rand1 = new Random(); 
-         * int indiceAttribut = rand1.nextInt(ListeListe.get(indiceListe).size()  );
-         * 
-         */
-         
-        
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
         
         int indiceAttribut =(int)(Math.random() * (ListeListe.get(indiceListe).size())); 
-
+        System.out.println("indiceattribut= "+indiceAttribut);
         
         //Question :
         Question Q = new Question(indiceListe,indiceAttribut,ListeListe.get(indiceListe).get(indiceAttribut));
-        
+        System.out.println(Q.toString());
         
         return Q;
     }
      
-<<<<<<< HEAD
     
-=======
-     
-    /* Méthode UpdateListes
-     * 
-     * @param : boolean Ans : réponse à la question donnée par l'utilisateur par l'interface
-     *          Question Q : la question auquelle l'utilisateur a répondu
-     * 
-     * Ne renvoie rien.
-     * 
-     * Utilise méthode MAJListes afin de reconstituer le "pool" de l'ordinateur, en fonction de la réponse à la Question posée.
-     */
-     
-     
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
     public void UpdateListes(boolean Ans,Question Q){
        
        
       
         //Pour chaque personnage dans liste des choix possibles
-        //on crée une  liste Personnage TEMPoraire pour pouvoir modifier ListePersonnagePossible DANS la boucle for each
+        //on crée une  liste Personnage pour pouvoir modifier ListePersonnagePossible DANS la boucle for each
         ArrayList<Personnage> ListePersonnageTEMP=new ArrayList<Personnage>();
         ListePersonnageTEMP=(ArrayList<Personnage>)(ListePersonnagePossibles.clone());
         
-        
+        System.out.println("//INFO DEBUG\\");
         for (Personnage P : ListePersonnagePossibles){
            
             //Valeur à éliminer ou à garder (Question posée par QuestionIA)
@@ -207,7 +103,7 @@ public class IA {
                 case 5 : V=P.getPays();break;
                 //case 4 : V=P.getCouleurPeau();break;
             }
-         
+            System.out.println(V+Q.getvaleurAttribut());
             
         
         
@@ -226,12 +122,12 @@ public class IA {
             
             
         }
-       
+        System.out.println("//FIN INFO DEBUG\\");
         
        
         ListePersonnagePossibles.clear();
         ListePersonnagePossibles=(ArrayList<Personnage>)(ListePersonnageTEMP.clone());
-        
+        System.out.println("Il reste "+ListePersonnagePossibles.size()+" personnages");
        
         //UPDATE FINAL DES LISTES
         MAJListes(ListePersonnagePossibles);
@@ -245,12 +141,9 @@ public class IA {
         ListeTypeCheveux.clear();
         ListeCouleurCheveux.clear();
         ListeCouleurYeux.clear();
+        //ListeCouleurPeau.clear();
         ListeAccessoire.clear();  
-        ListePays.clear(); 
-        //ListeCouleurPeau.clear(); enlevé
-        
-         
-        //Listes sans doublons
+        ListePays.clear();      
 		for(Personnage a : ListePersonnage){
 			if(!ListeGenre.contains(a.getGenre())){ListeGenre.add(a.getGenre());}
 			
@@ -276,7 +169,6 @@ public class IA {
         ListeListe.add(ListeAccessoire);
         ListeListe.add(ListePays);
         
-<<<<<<< HEAD
         
         //débuggage
           for(ArrayList A : ListeListe){
@@ -284,19 +176,11 @@ public class IA {
           }
         
         
-=======
-		
-        //ListeListe.add(ListeCouleurPeau); enlevé
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
         
         
     }
    
-<<<<<<< HEAD
    //POUR RENVOYER LE PERSONNAGE TROUVÉ
-=======
-   //Renvoie le personnage trouvé
->>>>>>> parent of f1e2595... Revert "Commentaire IA.java"
    public Personnage affichePersonnage(){
         return ListePersonnagePossibles.get(0);
       }
@@ -318,5 +202,7 @@ public class IA {
    
    }
    
+       
+	   
 }
 
